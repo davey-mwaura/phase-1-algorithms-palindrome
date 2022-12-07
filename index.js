ff16e63ac356914c09bcf5cd067c5fe806796aed
davey-mwaura <davidmburu50@gmail.com>
2022-12-07 03:39:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+
+  function isPalindrome(word) {
+    // 1
+    word = word.toLowerCase();
+  
+    // 2
+    const chars = word.split('');
+  
+    // 3
+    const charstwo = chars.reverse();
+  
+    // 4
+    const reversed = charstwo.join('');
+  
+    // 5
+    return word == reversed;
+  }
 
 /* 
-  Add your pseudocode here
+  Make a function that will make a reversed version of the inputed string then reverse it and compare it with the original string.
 */
 
-/*
-  Add written explanation of your solution here
+/* Here are the steps;
+  1.First, make the string lowercase so that it is not case-sensitive.
+  2.Then, use the split() method to split the string into an array of characters.
+    (This makes it easy to work with the individual characters)
+  3.Next, we use the reverse() method to reverse the order of the characters in the array.
+  4.Here, we use the join() method to convert the array of characters back into a string.
+  5.Finally compare the values!!!
 */
 
-// You can run `node index.js` to view these console logs
+console.log(isPalindrome('mom'));  // true
+console.log(isPalindrome('racecar'));  // true
+console.log(isPalindrome('hello'));  // false  
+
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
